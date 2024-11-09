6738677fbd252b2d55d348dcb4ffa9966bc338fe
Yashi Srivastava <yashisri2005@gmail.com>
2024-11-09 21:50:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.cpp
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    // Comment the part that outputs b
+    cout << a << " ";
+    //cout << b << " ";
+    cout << c << " ";
+    cout << endl;
+
+    return 0;
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
